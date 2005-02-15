4a2a3b373045459f9c446cd393a9118e6ab969af
rowen <rowen@b00c9580-4927-0410-8824-811d448ea4c7>
2005-02-15 22:18:42 +0000

    misc/EventLogger.py - Added fields "char" and "delta" - Modified two output names so all names exactly match   the corresponding attribute of the event. - Fixed an incompatibility with the current version of Tkinter   (some fields that were expected to be integers were not always so).
    git-svn-id: svn+ssh://sdss3.org/repo/ops/stui/tmp/trunk@117131 b00c9580-4927-0410-8824-811d448ea4c7

--- misc/EventLogger.py
+++ misc/EventLogger.py
@@ -1,53 +1,78 @@
 #!/usr/local/bin/python
 import Tkinter
 
-eventList = { '2': 'KeyPress', '3': 'KeyRelease', '4': 'ButtonPress',
-              '5': 'ButtonRelease', '6': 'Motion', '7': 'Enter',
-              '8': 'Leave', '9': 'FocusIn', '10': 'FocusOut',
-              '12': 'Expose', '15': 'Visibility', '17': 'Destroy',
-              '18': 'Unmap', '19': 'Map', '21': 'Reparent',
-              '22': 'Configure', '24': 'Gravity', '26': 'Circulate',
-              '28': 'Property',  '32': 'Colormap','36': 'Activate',
-              '37': 'Deactivate' }
+# dict of event type: event name
+eventDict = {
+	 '2': 'KeyPress',
+	 '3': 'KeyRelease',
+	 '4': 'ButtonPress',
+	 '5': 'ButtonRelease',
+	 '6': 'Motion',
+	 '7': 'Enter',
+	 '8': 'Leave',
+	 '9': 'FocusIn',
+	'10': 'FocusOut',
+	'12': 'Expose',
+	'15': 'Visibility',
+	'17': 'Destroy',
+	'18': 'Unmap',
+	'19': 'Map',
+	'21': 'Reparent',
+	'22': 'Configure',
+	'24': 'Gravity',
+	'26': 'Circulate',
+	'28': 'Property',
+	'32': 'Colormap',
+	'36': 'Activate',
+	'37': 'Deactivate',
+}
 
 root = Tkinter.Tk()
 
-def reportEvent(event):
-    rpt = '\n\n%s' % (80*'=')
-    rpt = '%s\nEvent: type=%s (%s)' %  (rpt, event.type,
-                                        eventList.get(event.type, 'Unknown'))
-    rpt = '%s\nserial=%s' %            (rpt, event.serial)
-    rpt = '%s  time=%s'   %            (rpt, event.time)
-    rpt = '%s  widget=%s' %            (rpt, event.widget)
-    rpt = '%s  height=%s' %            (rpt, event.height)
-    rpt = '%s  width=%s' %             (rpt, event.width)
-    rpt = '%s\nx=%d  y=%d'%            (rpt, event.x, event.y)
-    rpt = '%s  x_root=%d' %            (rpt, event.x_root)
-    rpt = '%s  y_root=%d' %            (rpt, event.y_root)
-    rpt = '%s\nnum=%s' %               (rpt, event.num)
-    rpt = '%s  keysym=%s' %            (rpt, event.keysym)
-    rpt = '%s  ksNum=%s' %             (rpt, event.keysym_num)
+def reportEvent(evt):
+	eventName = eventDict.get(evt.type, 'Unknown')
+	rptList = [
+		'\n\n%s\nEvent:' % (80*'=',),
+		' type=%r (%r' % (evt.type, eventName,),
+		'\nserial=%r' % (evt.serial,),
+		'time=%r' % (evt.time,),
+		'widget=%r' % (evt.widget,),
+		'\nx=%r  y=%r' % (evt.x, evt.y),
+		'x_root=%r' % (evt.x_root,),
+		'y_root=%r' % (evt.y_root,),
+		'\nnum=%r' % (evt.num,),
+		'char=%r' % (evt.char,),
+		'keysym=%r' % (evt.keysym,),
+		'keysym_num=%r' % (evt.keysym_num,),
+		'delta=%r' % (evt.delta,),
+		'\nheight=%r' % (evt.height,),
+		'width=%r' % (evt.width,),
+	]
 
-    #### some event types don't have these attributes 
-    try:
-        rpt = '%s  focus=%s' %   (rpt, event.focus)
-    except:
-        pass
-    try:
-        rpt = '%s  send=%s' %    (rpt, event.send_event)
-    except:
-        pass
-    
-    logWdg.yview("end")
-    logWdg.insert("end", rpt)
-    
+	#### some event types don't have these attributes 
+	try:
+		rptList.append(
+			'focus=%s' % (evt.focus)
+		)
+	except:
+		pass
+	try:
+		rptList.append(
+			'send_event=%s' % (evt.send_event)
+		)
+	except:
+		pass
+	
+	logWdg.yview("end")
+	logWdg.insert("end", '; '.join(rptList))
+	
 userLabel = Tkinter.Label(root, text="Type here:")
 userEntry  = Tkinter.Entry(root, width=10, takefocus=1, highlightthickness=2)
 logWdg = Tkinter.Text(root)
 
-for event in eventList.values():
-    userLabel.bind('<%s>' % event, reportEvent)
-    userEntry.bind('<%s>' % event, reportEvent)
+for eventName in eventDict.values():
+	userLabel.bind('<%s>' % eventName, reportEvent)
+	userEntry.bind('<%s>' % eventName, reportEvent)
 
 userLabel.grid(row=0, column=0)
 userEntry.grid(row=0, column=1, sticky='ew')
