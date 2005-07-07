cb18be2d60877d5fddc5ea79c070effa7ac0e29b
rowen <rowen@b00c9580-4927-0410-8824-811d448ea4c7>
2005-07-07 23:43:32 +0000

    RO/Comm/HTTPGet.py - Minor tweaks, including getting rid of a warning   when running the test code.
    git-svn-id: svn+ssh://sdss3.org/repo/ops/stui/tmp/trunk@117292 b00c9580-4927-0410-8824-811d448ea4c7

--- RO/Comm/HTTPGet.py
+++ RO/Comm/HTTPGet.py
@@ -23,7 +23,7 @@ import RO.AddCallback
 import RO.TkUtil
 
 _Debug = False
-_DebugExit = True
+_DebugExit = False
 
 class _ExitClass:
 	"""Class to keep track of outstanding nework transfers
@@ -192,16 +192,195 @@ class HTTPGet(RO.AddCallback.BaseMixin):
 		"""Add a function that will be called when the transfer completes"""
 		self._doneCallbacks.append(func)
 	
-	def _httpProgressCallback(self, token, totBytes, readBytes):
-		"""http callback function.
+	def removeDoneCallback(self, func):
+		"""Remove a done callback.
+		"""
+		self._doneCallbacks.remove(func)
+	
+	def start(self):
+		"""Start the download.
+		
+		If state is not Queued, raises RuntimeError
 		"""
 		if _Debug:
-			print "%s._httpProgressCallback(totBytes=%r, readBytes=%r)" % (self, totBytes, readBytes)
+			print "%s.start()" % (self,)
+		if self._state != self.Queued:
+			raise RuntimeError("Cannot start; state = %r not Queued" % (self.getState(),))
 
-		self._totBytes = int(totBytes)
-		self._readBytes = int(readBytes)
-		self._doCallbacks()
+		self._setState(self.Connecting)
+
+		try:
+			# verify output file and verify/create output directory, as appropriate
+			self._toPrep()
+	
+			# open output file
+			if _Debug:
+				print "HTTPGet: opening output file %r" % (self.toPath,)
+			if self.isBinary:
+				mode = "wb"
+			else:
+				mode = "w"
+			try:
+				self._tclFile = self._tkApp.call('open', self.toPath, mode)
+			except TclError, e:
+				raise RuntimeError("Could not open %r: %s" % (self.toPath, e))
+			self._createdFile = True
+			
+			# start http transfer
+			doneCallback = RO.TkUtil.TclFunc(self._httpDoneCallback, debug=_Debug)
+			progressCallback = RO.TkUtil.TclFunc(self._httpProgressCallback, debug=_Debug)
+			self._tclCallbacks = (doneCallback, progressCallback)
+			if _Debug:
+				print "HTTPGet: creating http connection"
+			self._tclHTTPConn = self._tkApp.call(
+				'::http::geturl', self.url,
+				'-channel', self._tclFile,
+				'-command', doneCallback,
+				'-progress', progressCallback,
+				'-binary', self.isBinary,
+				'-timeout', self.timeLimMS
+			)
+		except (SystemExit, KeyboardInterrupt), e:
+			self._setState(self.Failed, str(e))
+			raise
+		except Exception, e:
+			self._setState(self.Failed, str(e))
+			return
+
+		self._setState(self.Running)
+
+	def abort(self):
+		"""Start aborting: cancel the transaction and delete the output file.
+		Silently fails if the transaction has already completed
+		"""
+		if _Debug:
+			print "%s.abort()" % (self,)
+		if self.isDone():
+			return
+		elif self._state == self.Queued:
+			self._setState(self.Aborted)
+			return
+
+		if self._tclHTTPConn == None:
+			sys.stderr.write("HTTPGet cannot abort: isDone false but no http conn\n")
+			return
+
+		self._setState(self.Aborting)
+		self._tkApp.call("::http::reset", self._tclHTTPConn)
+		if _Debug:
+			print "http connection reset"
+
+	def getBytes(self):
+		"""Return bytes read and total bytes.
+		Warning: total bytes will be None if unknown.
+		"""
+		return (self._readBytes, self._totBytes)
+
+	def getErrMsg(self):
+		"""If the state is Failed, return an explanation.
+		Otherwise returns None.
+		"""
+		return self._errMsg
+	
+	def getState(self):
+		"""Returns the current state as an integer.
+		"""
+		return self._state
+	
+	def getStateStr(self, state=None):
+		"""Returns the state as a descriptive string.
 		
+		Inputs:
+		- state: state in question; if None then the current state is used.
+		"""
+		if state == None:
+			state = self._state
+		try:
+			return self.StateDict[state]
+		except KeyError:
+			return "Unknown (%r)" % (state)
+	
+	def isDone(self):
+		"""Returns True if the transaction is finished
+		(succeeded, aborted or failed), False otherwise.
+		"""
+		return self._state <= 0
+
+	def _setState(self, newState, errMsg=None):
+		"""Set a new state and call callbacks.
+		Do nothing if already done.
+		errMsg is ignored unless newState is Failed.
+
+		Raise RuntimeError if newState unknown.
+		"""
+		if _Debug:
+			print "%s._setState(newState=%s, errmsg=%r)" % (self, newState, errMsg)
+		# if state is not valid, reject
+		if self.isDone():
+			return
+	
+		if newState not in self.StateDict:
+			raise RuntimeError("Unknown state %r" % (newState,))
+		
+		self._state = newState
+		if newState == self.Failed:
+			self._errMsg = errMsg
+		
+		isDone = self.isDone()
+		if isDone:
+			self._cleanup()
+
+		self._doCallbacks()
+		if isDone:
+			# call done callbacks
+			# use a copy just in case the callback deregisters itself
+			for func in self._doneCallbacks[:]:
+				try:
+					func(self)
+				except (SystemExit, KeyboardInterrupt), e:
+					raise
+				except Exception, e:
+					sys.stderr.write("Callback of %s by %s failed: %s\n" % (func, self, e,))
+					traceback.print_exc(file=sys.stderr)
+		
+			# remove all callbacks
+			self._callbacks = []
+			self._doneCallbacks = []
+
+	def _cleanup(self):
+		"""Clean up everything except references to callbacks.
+		Private: call only from _setState!
+		
+		Close the input and output files and deregister the callbacks.
+		If state in (Aborted, Failed), delete the output file.
+		"""
+		if _Debug:
+			print "%s._cleanup()"
+		if self._tclHTTPConn != None:
+			self._tkApp.call("::http::cleanup", self._tclHTTPConn)
+			self._tclHTTPConn = None
+			if _Debug:
+				print "http connection cleaned up"
+		if self._tclFile:
+			self._tkApp.call("close", self._tclFile)
+			self._tclFile = None
+			if _Debug:
+				print "output file closed"
+		for tclFunc in self._tclCallbacks:
+			if _Debug:
+				print "deregister %s" % (tclFunc,)
+			tclFunc.deregister()
+		self._tclCallbacks = ()
+		
+		if self._createdFile and self._state in (self.Aborted, self.Failed):
+			try:
+				os.remove(self.toPath)
+				if _Debug:
+					print "deleted output file"
+			except OSError, e:
+				if _Debug:
+					print "failed to delete output file: %s" % (e,)
+
 	def _httpDoneCallback(self, token=None):
 		"""Called when the http transfer is finished.
 		"""
@@ -243,196 +422,17 @@ class HTTPGet(RO.AddCallback.BaseMixin):
 			if not errMsg:
 				errMsg = httpState
 		
-		self.setState(self.Done, errMsg)
+		self._setState(newState, errMsg)
 	
-	def removeDoneCallback(self, func):
-		"""Remove a done callback.
-		"""
-		self._doneCallbacks.remove(func)
-	
-	def start(self):
-		"""Start the download.
-		
-		If state is not Queued, raises RuntimeError
+	def _httpProgressCallback(self, token, totBytes, readBytes):
+		"""http callback function.
 		"""
 		if _Debug:
-			print "%s.start()" % (self,)
-		if self._state != self.Queued:
-			raise RuntimeError("Cannot start; state = %r not Queued" % (self.getState(),))
+			print "%s._httpProgressCallback(totBytes=%r, readBytes=%r)" % (self, totBytes, readBytes)
 
-		self.setState(self.Connecting)
-
-		try:
-			# verify output file and verify/create output directory, as appropriate
-			self._toPrep()
-	
-			# open output file
-			if _Debug:
-				print "HTTPGet: opening output file %r" % (self.toPath,)
-			if self.isBinary:
-				mode = "wb"
-			else:
-				mode = "w"
-			try:
-				self._tclFile = self._tkApp.call('open', self.toPath, mode)
-			except TclError, e:
-				raise RuntimeError("Could not open %r: %s" % (self.toPath, e))
-			self._createdFile = True
-			
-			# start http transfer
-			doneCallback = RO.TkUtil.TclFunc(self._httpDoneCallback, debug=_Debug)
-			progressCallback = RO.TkUtil.TclFunc(self._httpProgressCallback, debug=_Debug)
-			self._tclCallbacks = (doneCallback, progressCallback)
-			if _Debug:
-				print "HTTPGet: creating http connection"
-			self._tclHTTPConn = self._tkApp.call(
-				'::http::geturl', self.url,
-				'-channel', self._tclFile,
-				'-command', doneCallback,
-				'-progress', progressCallback,
-				'-binary', self.isBinary,
-				'-timeout', self.timeLimMS
-			)
-		except (SystemExit, KeyboardInterrupt), e:
-			self.setState(self.Failed, str(e))
-			raise
-		except Exception, e:
-			self.setState(self.Failed, str(e))
-			return
-
-		self.setState(self.Running)
-
-	def abort(self):
-		"""Start aborting: cancel the transaction and delete the output file.
-		Silently fails if the transaction has already completed
-		"""
-		if _Debug:
-			print "%s.abort()" % (self,)
-		if self.isDone():
-			return
-		elif self._state == self.Queued:
-			self.setState(self.Aborted)
-			return
-
-		if self._tclHTTPConn == None:
-			sys.stderr.write("HTTPGet cannot abort: isDone false but no http conn\n")
-			return
-
-		self.setState(self.Aborting)
-		self._tkApp.call("::http::reset", self._tclHTTPConn)
-		if _Debug:
-			print "http connection reset"
-
-	def getBytes(self):
-		"""Return bytes read and total bytes.
-		Warning: total bytes will be None if unknown.
-		"""
-		return (self._readBytes, self._totBytes)
-
-	def getErrMsg(self):
-		"""If the state is Failed, return an explanation.
-		Otherwise returns None.
-		"""
-		return self._errMsg
-	
-	def getState(self):
-		"""Returns the current state as an integer.
-		"""
-		return self._state
-	
-	def getStateStr(self, state=None):
-		"""Returns the state as a descriptive string.
-		
-		Inputs:
-		- state: state in question; if None then the current state is used.
-		"""
-		if state == None:
-			state = self._state
-		try:
-			return self.StateDict[state]
-		except KeyError:
-			return "Unknown (%r)" % (state)
-	
-	def isDone(self):
-		"""Returns True if the transaction is finished
-		(succeeded, aborted or failed), False otherwise.
-		"""
-		return self._state <= 0
-
-	def setState(self, newState, errMsg=None):
-		"""Set a new state and call callbacks.
-		Do nothing if already done.
-		errMsg is ignored unless newState is Failed.
-
-		Raise RuntimeError if newState unknown.
-		"""
-		if _Debug:
-			print "%s.setState(newState=%s, errmsg=%r)" % (self, newState, errMsg)
-		# if state is not valid, reject
-		if self.isDone():
-			return
-	
-		if newState not in self.StateDict:
-			raise RuntimeError("Unknown state %r" % (newState,))
-		
-		self._state = newState
-		if newState == self.Failed:
-			self._errMsg = errMsg
-		
-		isDone = self.isDone()
-		if isDone:
-			self.__cleanup()
-
+		self._totBytes = int(totBytes)
+		self._readBytes = int(readBytes)
 		self._doCallbacks()
-		if isDone:
-			# call done callbacks
-			# use a copy just in case the callback deregisters itself
-			for func in self._doneCallbacks[:]:
-				try:
-					func(self)
-				except (SystemExit, KeyboardInterrupt), e:
-					raise
-				except Exception, e:
-					sys.stderr.write("Callback of %s by %s failed: %s\n" % (func, self, e,))
-					traceback.print_exc(file=sys.stderr)
-		
-			# remove all callbacks
-			self._callbacks = []
-			self._doneCallbacks = []
-
-	def __cleanup(self):
-		"""Clean up everything except references to callbacks.
-		Private: call only from setState!
-		
-		Close the input and output files and deregister the callbacks.
-		If state in (Aborted, Failed), delete the output file.
-		"""
-		if _Debug:
-			print "%s._cleanup()"
-		if self._tclHTTPConn != None:
-			self._tkApp.call("::http::cleanup", self._tclHTTPConn)
-			self._tclHTTPConn = None
-			if _Debug:
-				print "http connection cleaned up"
-		if self._tclFile:
-			self._tkApp.call("close", self._tclFile)
-			self._tclFile = None
-			if _Debug:
-				print "output file closed"
-		for tclFunc in self._tclCallbacks:
-			if _Debug:
-				print "deregister %s" % (tclFunc,)
-			tclFunc.deregister()
-		self._tclCallbacks = ()
-		
-		if self._createdFile and self._state in (self.Aborted, self.Failed):
-			try:
-				os.remove(self.toPath)
-				if _Debug:
-					print "deleted output file"
-			except OSError, e:
-				if _Debug:
-					print "failed to delete output file: %s" % (e,)
 
 	def __str__(self):
 		return "%s(%s)" % (self.__class__.__name__, self.url)
@@ -470,7 +470,6 @@ if __name__ == "__main__":
 	testURL = "http://www.washington.edu/"
 	outFile = "httpget_test.html"
 	
-	global _Debug, _DebugExit
 	_Debug = False
 	_DebugExit = True
 	
