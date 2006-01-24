8f5e8c67a4da34b02cef4ba3efd6388bf13a92e1
rowen <rowen@b00c9580-4927-0410-8824-811d448ea4c7>
2006-01-24 19:15:51 +0000

    Modified to use py2app instead of bundlebuilder.
    git-svn-id: svn+ssh://sdss3.org/repo/ops/stui/tmp/trunk@117399 b00c9580-4927-0410-8824-811d448ea4c7

--- BuildForMac/setup.py
+++ BuildForMac/setup.py
@@ -1,14 +1,7 @@
 """The Python portion of the script that builds TUI
 
 Usage:
-% python setup.py py2app
-
-To do:
-- figure out if distDir is under control, and if so,
-  specify it (makes final copy more reasonable)
-- delete tcl docs at end or else comment out or remove the code to delete them
-- how much of Plist is useful and how much is replaced by py2app?
-  I'd like to specify shortversstr and getinfostr, but let py2app do the rest
+% python setup.py [--quiet] py2app
 
 History:
 2004-02-20 ROwen	Specify libs in buildapp instead of as cmd-line args.
@@ -25,7 +18,8 @@ History:
 2005-08-02 ROwen	Modified for the new TUI layout that allows the python code
 					to be zipped and separated from resources.
 2005-09-22 ROwen	Added TUI/Scripts to the list of resources.
-2006-01-20 ROwen	Renamed and modified to use py2app -- IN PROGRESS!!!
+2006-01-21 ROwen	Renamed from buildtui.py to setup.py.
+					Modified to use py2app.
 """
 from distutils.core import setup
 import py2app
@@ -47,21 +41,17 @@ tuiDir = os.path.dirname(os.path.abspath(TUI.__file__))
 ## it is likely to be replaced by a command-line argument.
 #bundlebuilder.USE_ZIPIMPORT = False
 
+appName = "TUI"
 mainProg = os.path.join(tuiRoot, "runtui.py")
-
-# Generate the full app name here
-# since it's needed for some file copying later.
+iconFile = "%s.icns" % appName
+contentsPath = os.path.join("dist", "%s.app" % (appName,), "Contents")
 versDate = TUI.Version.VersionStr
 appVers = versDate.split()[0]
-appName = "TUI"
-appPath = "dist/%s.app" % (appName,)
-iconFile = "%s.icns" % appName
 
 plist = Plist(
 	CFBundleName				= appName,
-#	CFBundleIconFile			= iconFile, # doesn't work; specify iconfile py2app option instead
 	CFBundleShortVersionString	= appVers,
-	CFBundleGetInfoString		= ' '.join([appName, versDate]),
+	CFBundleGetInfoString		= "%s %s" % (appName, versDate),
 	CFBundleExecutable			= appName,
 )
 
@@ -82,8 +72,8 @@ for resBase in resourceFiles:
 			(resSubPath, filePaths)
 		)
 	
-# Add tk snack to bogus location, then move it later
-# because these files get copied before the Tcl framework is built,
+# Add tk snack to simple bogus location, then move it later
+# because data_files get copied before the Tcl framework is built,
 # which screws up building the framework
 snackDir = "/Library/Tcl/snack2.2"
 data_files.append(
@@ -111,20 +101,18 @@ setup(
 )
 
 # move snack to its final location
-sourceDir = appPath + "/Contents/Resources/snack2.2"
-destDir = appPath + "/Contents/Frameworks/Tcl.Framework/Resources/snack2.2"
-print "rename %r to %r" % (sourceDir, destDir)
+sourceDir = os.path.join(contentsPath, "Resources", "snack2.2")
+destDir = os.path.join(contentsPath, "Frameworks", "Tcl.Framework", "Resources", "snack2.2")
+# print "rename %r to %r" % (sourceDir, destDir)
 os.rename(sourceDir, destDir)
 
-## Delete extraneous files
-#print "Deleting extraneous files:"
-#
-## Delete Tcl/Tk documentation
-## Note: needs revision for ActiveState Tcl
-#for pkgName in ["Tcl", "Tk"]:
-#	pkgPath = os.path.join(frameworkDir, "%s.framework" % (pkgName))
-#	for lprojPath in RO.OS.findFiles(pkgPath, "English.lproj", returnDirs=True):
-#		docPath = os.path.join(lprojPath, "Documentation")
-#		if os.path.exists(docPath):
-#			print "*", docPath
-#			shutil.rmtree(docPath)
+# Delete extraneous files
+print "*** deleting extraneous files ***"
+
+# Delete Tcl/Tk documentation
+docPath = os.path.join(contentsPath, "Frameworks", "Tcl.framework", "Resources", "English.lproj", "ActiveTcl-8.4")
+if os.path.exists(docPath):
+	print docPath
+	shutil.rmtree(docPath)
+else:
+	print "Warning: could not find %r" % (docPath,)
