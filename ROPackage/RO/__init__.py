7da277917b884556868737f944551a08d55e00af
rowen <rowen@b00c9580-4927-0410-8824-811d448ea4c7>
2007-01-24 21:54:59 +0000

    RO did not have attribute __version__
    git-svn-id: svn+ssh://sdss3.org/repo/ops/stui/tmp/trunk@117576 b00c9580-4927-0410-8824-811d448ea4c7

--- ROPackage/RO/__init__.py
+++ ROPackage/RO/__init__.py
@@ -1,5 +1,6 @@
 """Various utilities written, adapted or borrowed by Russell Owen
 
-See README.txt for information and license
+See ../README.txt and ../docs of source code
+for information and license
 """
-from Version import *
+from Version import __version__
