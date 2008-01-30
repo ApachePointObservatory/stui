d847b35f16919b2b2e7216337a7573c87aa012e8
rowen <rowen@b00c9580-4927-0410-8824-811d448ea4c7>
2008-01-30 22:43:44 +0000

    RO version 2.2.6 This is the first version uploaded to PyPI
    ROPackage/setup.py
    - Added metadata for PyPI
    
    
    
    git-svn-id: svn+ssh://sdss3.org/repo/ops/stui/tmp/trunk@117691 b00c9580-4927-0410-8824-811d448ea4c7

--- ROPackage/setup.py
+++ ROPackage/setup.py
@@ -6,6 +6,27 @@ python setup.py install
 
 Alternatively you can copy python/RO to site-packages
 """
+
+long_description = """\
+A package of cross-platform utilities, including:
+* Alg: various useful algorithms including OrderedDict (a dictionary that remembers the order in which items are added) and MultiDict (a dictionary whose values are lists).
+* Astro: astronomical calculations including conversion between various coordinate systems and time functions. Note that this library is based on an extensive body of work by Pat Wallace and as such requires payment for commercial use.
+* Comm: network communications based on tcl sockets (to mesh nicely with Tkinter applications).
+* DS9: interface to SAO's ds9 image viewer. Images can be loaded from numpy arrays, binary files or fits files.
+* OS: additions to the standard "os" module (especially os.path). Includes getPrefsDir (find the standard preferences directory on Mac, Windows or unix), findFiles (recursively search for files whose names match specified patterns and don't match others), and removeDupPaths (remove duplicates from a list of paths).
+* Prefs: a complete Tkinter-based preferences implementation.
+* ScriptRunner: execute user-written scripts that wait for something to happen without halting the main Tkinter event loop. Based on generators.
+* SeqUtil: sequence utilities, such as isSequence (returns True if the argument is a sequence), asList (converts one item or a sequence of items to a list), etc.
+* StringUtil: string utilities including conversion of sexagesimal (dd:mm:ss and hh:mm:ss) strings to and from numbers.
+* Wdg: extensions of the standard Tkinter widgets and useful additional widgets. The widgets support hot help strings (automatically displayed in an RO.Wdg.StatusBar), linking to html help and a contextual menu.
+"""
+
+classifiers = """\
+Development Status :: 5 - Production/Stable
+Programming Language :: Python
+Topic :: Software Development :: Libraries :: Python Modules
+"""
+
 from setuptools import setup, find_packages
 import sys
 import os
@@ -23,9 +44,14 @@ print "%s version %s" % (PkgName, Version.__version__)
 setup(
     name = PkgName,
     version = Version.__version__,
-    description = "Utility package including astronomical transforms and Tkinter widgets",
+    description = "Collection of utilities including general algorithms, astronomical transformations and Tkinter widgets",
+    long_description = long_description,
     author = "Russell Owen",
+    author_email = "rowen@u.washington.edu",
     url = "http://www.astro.washington.edu/rowen/",
+    license = "GPL except RO.Astro which has more restrictions for commercial use",
+    classifiers = filter(None, classifiers.split("\n")),
+    platforms = ["MacOS X", "unix", "Windows"],
     install_requires = ['numpy'],
     package_dir = {PkgName: PkgDir},
     packages = find_packages(PkgRoot),
