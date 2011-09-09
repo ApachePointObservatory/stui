72ff9f8e86b01527c9e07a1a3f1e980eeecbfe75
rowen <rowen@b00c9580-4927-0410-8824-811d448ea4c7>
2011-09-09 22:25:49 +0000

    Made animation do a better job of testing exposure table.
    git-svn-id: svn+ssh://sdss3.org/repo/ops/stui/trunk@126838 b00c9580-4927-0410-8824-811d448ea4c7

--- TUI/Inst/APOGEEQL/TestData.py
+++ TUI/Inst/APOGEEQL/TestData.py
@@ -1,6 +1,6 @@
 import TUI.Base.TestDispatcher
 
-testDispatcher = TUI.Base.TestDispatcher.TestDispatcher("apogeeql", delay=3)
+testDispatcher = TUI.Base.TestDispatcher.TestDispatcher("apogeeql", delay=2)
 tuiModel = testDispatcher.tuiModel
 
 ICCDataList = (
@@ -18,10 +18,6 @@ MainDataList = (
     "utrData=00120013, 3, 20.0, -99.05, 174.10, -98.70, 171.90, 0, 0.51, 0.50, 1.0, 633.0, 6, 6, 5.0, Object, B",
     "utrData=00120013, 4, 24.0, -99.05, 174.10, -98.70, 171.90, 0, 0.51, 0.50, 1.0, 633.0, 6, 6, 4.0, Object, B",
     "snrAxisRange=14, 32",
-#    "predictedExposure=1001, 1, 00120013, 600.0, 5, 30.0, Object, 0.5, A",
-#     "predictedExposure=1001, 2, 00120013, 600.0, 5, 30.0, Object, 0.5, A",
-#     "predictedExposure=1001, 3, 00120014, 600.0, 5, 30.0, Object, 0.5, B",
-#     "predictedExposure=1001, 4, 00120014, 600.0, 5, 30.0, Object, 0.5, B",
     "predictedExposure=1001, 5, 00120013, 600.0, 5, 30.0, Object, 0.5, A",
     "predictedExposure=1001, 6, 00120014, 600.0, 5, 30.0, Object, 0.5, B",
     "missingFibers=00120013, 4, 0",
@@ -29,10 +25,16 @@ MainDataList = (
 
 AnimDataSet = (
     (
+        "exposureData=1002, 1, 00120009, 600.0, 5, 30.0,  0.0, 28.2,  600, 28.2, Object, ?",
         "utrData=00120013, 5, 28.0, -99.05, 174.10, -98.70, 171.90, 15, 0.51, 0.50, 1.0, 633.0, 6, 6, 4.0, , Object, B",
         "missingFibers=00120013, 4, nan",
     ),
     (
+        "predictedExposure=1002, 2, 00120013, 600.0, 5, 30.0, Object, 0.5, A",
+        "predictedExposure=1002, 3, 00120014, 600.0, 5, 30.0, Object, 0.5, B",
+        "predictedExposure=1002, 4, 00120014, 600.0, 5, 30.0, Object, 0.5, B",
+        "predictedExposure=1002, 5, 00120013, 600.0, 5, 30.0, Object, 0.5, A",
+        "predictedExposure=1002, 6, 00120014, 600.0, 5, 30.0, Object, 0.5, B",
         "missingFibers=00120013, 4, 3, 2, 57, 75",
     ),
 )
