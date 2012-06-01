d765c94893131b360e4088184dda4f63718c0f1f
rowen <rowen@b00c9580-4927-0410-8824-811d448ea4c7>
2012-06-01 17:25:27 +0000

    Improve BOSSMonitorWindow by removing camera controls and making it all automatic
    git-svn-id: svn+ssh://sdss3.org/repo/ops/stui/trunk@133852 b00c9580-4927-0410-8824-811d448ea4c7

--- TUI/Inst/GuideMonitor/BOSSMonitorWindow.py
+++ TUI/Inst/GuideMonitor/BOSSMonitorWindow.py
@@ -44,52 +44,33 @@ class BOSSTemperatureMonitorWdg(Tkinter.Frame):
             height = height,
             cnvTimeFunc = TUI.Base.StripChartWdg.TimeConverter(useUTC=True),
         )
-        self.stripChartWdg.grid(row=1, column=0, sticky="nwes")
-        self.grid_rowconfigure(1, weight=1)
+        self.stripChartWdg.grid(row=0, column=0, sticky="nwes")
+        self.grid_rowconfigure(0, weight=1)
         self.grid_columnconfigure(0, weight=1)
 
         # the default ticks are not nice, so be explicit
         self.stripChartWdg.xaxis.set_major_locator(matplotlib.dates.MinuteLocator(byminute=range(0, 61, 2)))
 
-        buttonFrame = Tkinter.Frame(self)
-        
-        RO.Wdg.StrLabel(
-            master = buttonFrame,
-            text = "Show:",
-        ).pack(side="left")
         self.cameraNameList = (
             "sp1r0",
             "sp1b2",
             "sp2r0",
             "sp2b2",
         )
-        self.cameraColorDict = {
+        self.cameraNameColorDict = {
             "sp1r0": "red",
             "sp1b2": "green",
             "sp2r0": "blue",
             "sp2b2": "black",
         }
         
-        self.tempLinesDict = {}
+        self.nomTempColorDict = dict() # dict of camera name: color
+        self.nomTempLineDict = dict() # dict of camera name: nominal temperature line (if present)
+        self.readTempLineDict = dict() # dict of camera name: read temperature line
         for cameraName in self.cameraNameList:
-            self.tempLinesDict[cameraName] = []
+            self.addTemperatureLines(cameraName)
         
-        self.cameraWdgList = []
-        for cameraName in self.cameraNameList:
-            cameraWdg = RO.Wdg.Checkbutton(
-                master = buttonFrame,
-                text = cameraName,
-                defValue = True,
-                helpText = "show/hide temperature for %s" % (cameraName,),
-                callFunc = self.showHideCameraLines,
-            )
-            cameraWdg.pack(side="left")
-            self.cameraWdgList.append(cameraWdg)
-
-        buttonFrame.grid(row=0, column=0, sticky="w")
         
-        self.showHideCameraLines()
-                        
         self.stripChartWdg.showY(-140.0, -90.0, subplotInd=0)
         self.stripChartWdg.subplotArr[0].legend(loc=3, frameon=False)
         self.stripChartWdg.subplotArr[0].yaxis.set_label_text("CCDTemp (C)")
@@ -111,51 +92,59 @@ class BOSSTemperatureMonitorWdg(Tkinter.Frame):
         self.stripChartWdg.addConstantLine(10.0, subplotInd=1, color="grey")
         self.stripChartWdg.showY(0.1, 10.5, subplotInd=1)
         self.stripChartWdg.subplotArr[1].legend(loc=3, frameon=False)
-        self.stripChartWdg.subplotArr[1].yaxis.set_label_text("Ln2")
+        self.stripChartWdg.subplotArr[1].yaxis.set_label_text("Ln2 Pressure")
+
+        self.clearWdg = RO.Wdg.Button(master = self, text = "C", callFunc = self.stripChartWdg.clear)
+        self.clearWdg.grid(row=0, column=0, sticky = "sw")
     
-    def showHideCameraLines(self, dum=None):
-        """Show or hide all camera lines
+    def nomTempCallback(self, keyVar, cameraName):
+        """Draw a constant line for a new value of a nominal temperature
         """
-        for wdg in self.cameraWdgList:
-            cameraName = wdg["text"]
-            lineList = self.tempLinesDict[cameraName]
-            
-            if wdg.getBool():
-                if lineList:
-                    continue # lines already exist
-
-                lineList[:] = self.addLines(cameraName = cameraName)
-            else:
-                if not lineList:
-                    continue # no lines to remove
-
-                for line in lineList:
-                    self.stripChartWdg.removeLine(line)
-                lineList[:] = []
+        color = self.cameraNameColorDict[cameraName]
+        line = self.nomTempLineDict.pop(cameraName, None)
+        if line:
+            self.stripChartWdg.removeLine(line)
+        
+        nomTemp = keyVar[0]
+        if nomTemp == None:
+            return
+        
+        line = self.stripChartWdg.addConstantLine(
+            nomTemp,
+            color = color,
+            linestyle = "--",
+        )
+        self.nomTempLineDict[cameraName] = line
+        
+        readLine = self.readTempLineDict[cameraName]
+        readLine.line2d.set_label("%s (%0.1f)" % (cameraName, nomTemp))
+        self.stripChartWdg.subplotArr[0].legend(loc=3, frameon=False)
+        
      
-    def addLines(self, cameraName):
-        """Add lines for a given camera; does not check if they already exist
+    def addTemperatureLines(self, cameraName):
+        """Add read temperature line for a given camera; does not check if it already exists
+        
+        Also sets a callback for nominal temperature to draw a constant line
         """
-        lineList = []
-        lineColor = self.cameraColorDict[cameraName]
         uprCameraName = cameraName.upper()
+
         tempNomKeyVar  = getattr(self.bossModel, "%sCCDTempNom"  % (uprCameraName,))
+        def callFunc(keyVar, self=self, cameraName=cameraName):
+            self.nomTempCallback(keyVar, cameraName=cameraName)
+        tempNomKeyVar.addCallback(callFunc)
+
         tempReadKeyVar = getattr(self.bossModel, "%sCCDTempRead" % (uprCameraName,))
 
-        constVal = tempNomKeyVar[0]
-        if constVal != None:
-            constLine = self.stripChartWdg.addConstantLine(constVal, subplotInd=0, color="grey")
-            lineList.append(constLine)
-            
+        color = self.cameraNameColorDict[cameraName]
         line = self.stripChartWdg.plotKeyVar(
             label = cameraName,
             subplotInd = 0,
             keyVar = tempReadKeyVar,
             keyInd = 0,
-            color = lineColor,
+            color = color,
         )
-        lineList.append(line)
-        return lineList
+        self.readTempLineDict[cameraName] = line
+        return line
  
 
 if __name__ == "__main__":
