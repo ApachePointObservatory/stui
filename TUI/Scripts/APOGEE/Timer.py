23d8817710be34a045316ad4a9ba98275ffe5c9b
elenam <elenam@b00c9580-4927-0410-8824-811d448ea4c7>
2013-04-22 04:51:54 +0000

    apogeeTimer
    git-svn-id: svn+ssh://sdss3.org/repo/ops/stui/trunk@145596 b00c9580-4927-0410-8824-811d448ea4c7

--- TUI/Scripts/APOGEE/Timer.py
+++ TUI/Scripts/APOGEE/Timer.py
@@ -1,70 +1,81 @@
+#  sop-apogee timer
+#  history
+#  added say alert 
+# 03/11/2013:  EM: changed voice to mac system Glass.wav
+# 04/20/2013:  EM: multiple refinement,  added format and colors,  
+#  changed time left for None if no exposures left in sop sequence. 
+
 import RO.Wdg
 import TUI.Models
-#import Tkinter
-import time
-seqState=0
-utrState=1
 
 class ScriptClass(object):
     def __init__(self, sr, ):
 
        sr.master.winfo_toplevel().wm_resizable(True, True)
-       self.labWdg = RO.Wdg.Label(master=sr.master, text ="           ")
-       self.labWdg.grid(row=0, column=0, sticky="nsw")
-              
- #      self.logWdg = RO.Wdg.LogWdg(master=sr.master,width=50,height =3,)
- #      self.logWdg.grid(row=0, column=0, sticky="nsew")
+       self.ft=("Times", "16", "bold") 
+       self.labWdg = RO.Wdg.Label(master=sr.master, text ="           ", font=self.ft, fg="black")
+       self.labWdg.grid(row=0, column=0, sticky="nsw")                           
        sr.master.rowconfigure(0, weight=1)
        sr.master.columnconfigure(0, weight=1)
 
-       print "---start --" 
+       sPath="/Library/Application Support/STUIAdditions"
+       ffid=sPath+"/Scripts/Glass.wav"
+       self.soundFid = RO.Wdg.SoundPlayer(ffid)
+
        self.sr=sr 
+       self.alertTime=5.0
+       self.alert=True
        self.sopModel = TUI.Models.getModel("sop")
-       self.sopModel.doApogeeScience_sequenceState.addCallback(self.seqState,callNow=True)
-
        self.apogeeModel = TUI.Models.getModel("apogee")
+       self.utr2=self.apogeeModel.utrReadState[2]
+       self.utrTimeOver=self.utr2*self.apogeeModel.utrReadTime[0]  
+       self.sopModel.doApogeeScience_sequenceState.addCallback(self.seqState,callNow=True)
        self.apogeeModel.utrReadState.addCallback(self.utrState,callNow=True)
-
-     #  self.record()
-         
+       self.record()
+                
     def seqState(self, keyVar): 
         if not keyVar.isGenuine: return
-     #   global seqState
-     #   seqState=1
-    #   sr=self.sr
         self.record()
 
     def utrState(self, keyVar):
          if not keyVar.isGenuine: return
-       #  global utrState
-       #  utrState=1
-       #  sr=self.sr
-         self.record()
+         if keyVar[1] == "Done" and keyVar[2] != self.utr2: 
+             self.utr2=keyVar[2]
+             expTime=self.apogeeModel.utrReadTime[0]
+             self.utrTimeOver=self.utr2*expTime
+             self.record()
 
     def record(self,):
          sr=self.sr
-       #  sq=sr.getKeyVar(self.sopModel.doApogeeScience_sequenceState,ind=0, defVal="DDDD")
-         sq=self.sopModel.doApogeeScience_sequenceState[0]
-       #  expTime=sr.getKeyVar(self.sopModel.doApogeeScience_expTime, ind=0, defVal=500)
-         expTime=self.sopModel.doApogeeScience_expTime[0]
-         indState=self.sopModel.doApogeeScience_sequenceState[1]
-         t1=len(sq)*expTime  # total exposure time
-         t2=indState*expTime  #  full number of exposures completed 
-         utr2=self.apogeeModel.utrReadState[2]
-         tm=self.apogeeModel.utrReadTime[0]
-         tover=t2+utr2*tm
-         self.labWdg.set("Apogee exposure time left  = %5.1f min" % ((t1-tover)/60))
-         
-       #  return "Record"
+         sq, indState =self.sopModel.doApogeeScience_sequenceState[0:2]   
+              #  sq -  sequence ABBAABBA, indState - number of A/B completed  
+         if len(sq)==indState: 
+             self.labWdg.set("  SOP/Apogee time left  = None")
+             return 
 
-    def getTAITimeStr(self,):
-        return time.strftime("%H:%M",
-              time.gmtime(time.time() -  - RO.Astro.Tm.getUTCMinusTAI()))
-            
+         expTime=self.sopModel.doApogeeScience_expTime[0]   # time for one A or B  
+         sqTime=len(sq)*expTime  # full time requested, len(ABBAABBA)* expTime                    
+         sqTimeOver=indState*expTime  #  time over for completed some of ABBAABBA
+           
+         timeLeft=(sqTime - sqTimeOver - self.utrTimeOver)/60.0
+         self.labWdg.set("  SOP/Apogee time left  =  %5.1f min" % (timeLeft))         
+         if (timeLeft > self.alertTime):
+              self.alert=True;  fg="darkgreen"
+         elif (0 < timeLeft <= self.alertTime):
+              fg="red" 
+              if  self.alert: 
+                    self.alert = False;  
+                    self.soundFid.play() 
+         else:  fg="black" 
+         self.labWdg.config(fg=fg)
+         return
+                          
     def run(self, sr):
         self.record()
-        pass
                 
     def end(self, sr): 
-        print "--end--"
+        pass        
+      #  self.sopModel.doApogeeScience_sequenceState.removeCallback(self.seqState)
+      #  self.apogeeModel.utrReadState.removeCallback(self.utrState)
+      #  print "-- call removed,  done --"
          
