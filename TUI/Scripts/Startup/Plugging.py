838704761a5792344a4ccbb177aa33f7b09878f5
elenam <elenam@b00c9580-4927-0410-8824-811d448ea4c7>
2014-02-20 06:42:22 +0000

    updated Plugging script: cartridge 2 instead of 18
    git-svn-id: svn+ssh://sdss3.org/repo/ops/stui/trunk@154373 b00c9580-4927-0410-8824-811d448ea4c7

--- TUI/Scripts/Startup/Plugging.py
+++ TUI/Scripts/Startup/Plugging.py
@@ -1,6 +1,7 @@
 """
 History:  created by EM to list plugged plates and to check if plates duplicated
-02-06-2013 EM adjusted window size; added version date, moved to ..more directory
+02-06-2013 EM adjusted window size; added version date, moved to STUI scripts. 
+02/19/2014 EM cart 18 is modified to cart 2. Updated the list of available cartridges.
 """
 
 import RO.Wdg
@@ -24,18 +25,15 @@ class ScriptClass(object):
         self.logWdg.text.tag_config("a", foreground="magenta") 
         self.logWdg.text.tag_config("g", foreground="darkblue") 
 
-        self.ct=[1,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,]
+        self.ct=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17]
         n=len(self.ct)
         self.pl=["...."]*n        
-        hlp="new plugging"; 
-        th="put to cartridge bay"
 
         self.platedbModel = TUI.Models.getModel("platedb")
 
-  #      fs="12"   # font size
-  #      ft="Monaco" # "Courier"  #"Menlo"  # font type
-  #      self.logWdg.text.tag_config("cur", font=(ft,fs))        
-  #      self.logWdg.text.tag_config("cur1", font=(ft,fs,"bold"))
+        fs="12"   # font size
+        ft="Monaco" # "Courier"  #"Menlo"  # font type
+        self.logWdg.text.tag_config("cur", font=(ft,fs))        
         
     def updatePlugging(self, keyVar): 
         if not keyVar.isGenuine: return
@@ -49,12 +47,10 @@ class ScriptClass(object):
         
     def run(self, sr):
         timeStr = self.getTAITimeStr()
-        self.logWdg.addMsg("%s,  %s" % (sr.name, timeStr), tags=["a"])
-   #     self.logWdg.addMsg("platedb showActive ", tags=["cur1"])
-        self.logWdg.addMsg("platedb showActive ")
+        self.logWdg.addMsg("platedb showActive, %s" % (timeStr), tags=["a"])
         
         self.platedbModel.activePlugging.addCallback(self.updatePlugging,callNow=True)
-        yield sr.waitCmd(actor="platedb", cmdStr="showActive", abortCmdStr="abort", checkFail=False)
+        yield sr.waitCmd(actor="platedb", cmdStr="showActive", abortCmdStr="abort", checkFail=True)
         self.platedbModel.activePlugging.removeCallback(self.updatePlugging)
         
         n=len(self.ct)
@@ -66,18 +62,18 @@ class ScriptClass(object):
     # test code
     #    self.pl[15]=4013;  self.pl[16]=4013
         for i in range(0,n):
+            pl=self.pl[i];  ct=self.ct[i]
             for j in range(i+1,n):
-                pl=self.pl[i];  ct=self.ct[i]
                 pl1=self.pl[j];   ct1=self.ct[j]
                 if (pl != "....") and (pl1 != "...."): 
                     if int(pl)==int(pl1) :
                         err=1
-                        self.logWdg.addMsg("(%s/%s) == (%s/%s)" %
+                        self.logWdg.addMsg("(%s/%s) == (%s/%s)" % \
                                    (ct, pl,ct1, pl1),severity=RO.Constants.sevError, tags=["cur"])
         if err == 0:
             self.logWdg.addMsg("No duplicated plates - ok!")
         else:
-            self.logWdg.addMsg("Error:  same plates on diff carts ",
+            self.logWdg.addMsg("Error:  same plates on different carts ",
                                severity=RO.Constants.sevError, tags=["cur"])
         self.logWdg.addMsg("-- done --", tags=["a"])
 
