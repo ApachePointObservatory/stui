fb730c02c664e3afc93c34306f0736ed76c9051e
José Sánchez-Gallego <gallegoj@uw.edu>
2020-11-25 22:56:07 -0800

    Remove scripts

--- TUI/Scripts/Closing/Quick Close.py
+++
@@ -1,5 +0,0 @@
-def run(sr):
-# quickClose.py
-  from datetime import datetime
-  tt=datetime.utcnow()
-  yield sr.waitCmd(actor="tcc", cmdStr="track 121,30 mount")
--- TUI/Scripts/Tutorial/1 Hello.py
+++
@@ -1,7 +0,0 @@
-class ScriptClass(object):
-    def __init__(self, sr):
-        pass
-
-    def run(self, sr):
-        sr.showMsg("Hello")
-        yield sr.waitMS(1000)
