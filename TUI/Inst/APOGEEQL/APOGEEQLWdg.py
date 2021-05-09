81ffd89f2edfba935943da902dd456327eec008a
José Sánchez-Gallego <gallegoj@uw.edu>
2021-05-09 12:31:17 -0700

    Disable QuickLook S/N plot

--- TUI/Inst/APOGEEQL/APOGEEQLWdg.py
+++ TUI/Inst/APOGEEQL/APOGEEQLWdg.py
@@ -29,20 +29,20 @@ class APOGEEQLWdg(Tkinter.Frame):
         row = 0
         self.readStatusWdg = ReadStatusWdg.ReadStatusWdg(master=self, helpURL=_HelpURL)
         self.readStatusWdg.grid(row=row, column=0, sticky="nw")
-        
+
         sepFrame = Tkinter.Frame(self, bg="gray")
         sepFrame.grid(row=row, column=1, padx=2, sticky="ns")
-        
+
         self.expLogWdg = ExposureTableWdg.ExposureTableWdg(master=self, helpURL=_HelpURL)
         self.expLogWdg.grid(row=row, column=2, sticky="news")
         row += 1
-        
+
         self.statusBar = TUI.Base.Wdg.StatusBar(master=self)
         self.statusBar.grid(row=row, column=0, columnspan=3, sticky="ew")
         row += 1
-        
-        self.snrGraphWdg = SNRGraphWdg.SNRGraphWdg(master=self, width=_Width, height=_Height, helpURL=_HelpURL)
-        self.snrGraphWdg.grid(row=row, column=0, columnspan=3, sticky="news")
+
+        # self.snrGraphWdg = SNRGraphWdg.SNRGraphWdg(master=self, width=_Width, height=_Height, helpURL=_HelpURL)
+        # self.snrGraphWdg.grid(row=row, column=0, columnspan=3, sticky="news")
         self.grid_rowconfigure(row, weight=1)
         self.grid_columnconfigure(2, weight=1)
         row += 1
--- TUI/Version.py
+++ TUI/Version.py
@@ -1,3 +1,3 @@
-VersionStr = "1.7.12 2020-11-30"
+VersionStr = "1.7.13b0 2021-05-09"
 VersionName, VersionDate = VersionStr.split()
 ApplicationName = "STUI"
